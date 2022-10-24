6b35e4b971caa88acc3eade59bcc2e40f9cb6512
esanchezgomez <116447812+esanchezgomez@users.noreply.github.com>
2022-10-24 20:20:38 +0200

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,21 +2,23 @@
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance
 
 resource "aws_instance" "ppro_web_server" {
-    ami                         = "ami-0a20b6d46b59a5cd5"
+    ami                         = "ami-0493936afbe820b28"
     instance_type               = "t2.micro"
     subnet_id                   = aws_subnet.ppro_public_subnet_1.id
     vpc_security_group_ids      = aws_security_group.allow_tls_rdp_in.id
     associate_public_ip_address = true
-    key_name                    = aws_key_pair.ppro_key_pair.id
-    user_data                   = data.template_file.install-iis.rendered
+    #key_name                    = aws_key_pair.ppro_key_pair.id
+    user_data                   = data.template_file.hello-world.rendered
     #user_data                  = "${file("install_apache.sh")}"
 }
 
-data "template_file" "install-iis" {
-    template = <<EOF
-    <powershell>
-    Install-WindowsFeature -Name Web-Server -IncludeManagementTools;
-    Set-Service -Name W3SVC -StartupType Automatic;
-    </powershell>
+# Simple bash script that writes "Hello, World" into index.html and runs a tool called busybox (installed by default on Ubuntu)
+# to spin up a web server on port 8080
+
+data "template_file" "hello-world" {
+    template = <<-EOF
+    #!/bin/bash
+    echo "Hello, World" > index.html
+    nohup busybox httpd -f -p 8080 &
     EOF
 }
